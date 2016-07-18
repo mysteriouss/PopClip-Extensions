4f36b80b89ad92a7f4bd102adc3fdbbf86aec2af
mysteriouss <mysteriouss@126.com>
2016-07-18 19:39:09 +0800

    IPCN with ipip.net

--- IPCN.popclipext/ipcn.php
+++ IPCN.popclipext/ipcn.php
@@ -20,18 +20,43 @@ if(filter_var($ipaddr, FILTER_VALIDATE_IP)){ //, FILTER_FLAG_NO_PRIV_RANGE | FIL
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
   curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_USERAGENT, 'curl/7.43.0');
   $data = curl_exec($ch);
   $status_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
   curl_close($ch);
-  if($status_code === 200 && !is_null($data)){
+  if($status_code == 200 && !is_null($data)){ // && false
     if (!empty($data)) {
         echo $data;
     } else {
         echo 'empty: '.$ipaddr;
     }
   }else{
-    echo 'invalid: ' . $ipaddr;
+    //echo 'invalid: ' . $ipaddr;
+    $api = 'http://freeapi.ipip.net/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$api . $ipaddr);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'curl/7.43.0');
+    $data = curl_exec($ch);
+    $status_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if($status_code === 200 && !is_null($data)){
+      if (!empty($data)) {
+        $json = json_decode($data,true);
+        if(is_array($json)){
+          echo 'IP: '.$ipaddr . ' 来自: '. implode(' ', $json);
+        }else{
+          echo $data;
+        }
+      } else {
+          echo 'empty: ' .$ipaddr;
+      }
+    }else{
+      echo 'invalid: ' .$ipaddr;
+    }
   }
 }else{
   $ch = curl_init();
