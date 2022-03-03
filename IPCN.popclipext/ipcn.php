26c38b48855f4e5ac4d7a94cc35bc6a5dc2a7cfa
mysteriouss <mysteri0uss@outlook.com>
2022-03-03 08:45:35 +0800

    Update ipcn.php

--- IPCN.popclipext/ipcn.php
+++ IPCN.popclipext/ipcn.php
@@ -1,8 +1,8 @@
 <?php
 $input = getenv('POPCLIP_TEXT');
-$ipaddr = $input;
+$ipaddr = trim($input);
 if(filter_var($ipaddr, FILTER_VALIDATE_IP)){ //, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE
-  ipip_net($ipaddr);
+  haoip_cn($ipaddr);
   return;
 }
 
@@ -50,6 +50,38 @@ function ipip_net($ipaddr){
     }
   } else {
       echo 'ipip.net empty: ' .$ipaddr;
+  }
+}
+
+function haoip_cn($ipaddr){
+  if(!$ipaddr) return;
+
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, "https://haoip.cn/ip/$ipaddr");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+
+  $headers = array();
+  $headers[] = 'User-Agent: curl/7.78.0';
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+  $data = curl_exec($ch);
+  if (curl_errno($ch)) {
+      echo 'Error:' . curl_error($ch);
+  }
+  curl_close($ch);
+
+  if (!empty($data)) {
+    $json = json_decode($data,true);
+    if(is_array($json)){
+      echo 'IP: '.$ipaddr . ' 来自: '. implode(' ', $json). '「haoip.cn」';
+    }else{
+      echo str_replace(array("\r", "\n"), ' ', $data);
+    }
+  } else {
+      echo 'haoip.cn empty: ' .$ipaddr;
   }
 }
 
